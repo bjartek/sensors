9102936c4792c50b36bdccac7aa953aadce3f040
Bjarte Stien Karlsen <bjarte@bjartek.org>
2015-04-08 21:51:31 +0200

    remove old test script

--- example-post.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -H "Content-Type: application/vnd.collection+json" -X POST -d '{"template":{"data":[{"name":"name","prompt":"Name","value":"TestSensor"},{"name":"location","prompt":"Location","value":"TestLocation"}]}}' http://localhost:8080/sensors
-
